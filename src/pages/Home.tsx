e52b5f6231352fb37a9aefd9800ad18b8c6458b9
Océlia Sousa <oceliaassis@gmail.com>
2023-09-17 22:16:45 -0300

    feat: created home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <main>
+      <p data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+    </main>
+  );
+}
+
+export default Home;
